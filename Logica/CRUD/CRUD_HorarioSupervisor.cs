7117fdc729ee74ecc64f312f93b062f8509bdd29
Isaac Jacome <isaacjacome9@gmail.com>
2023-04-25 00:23:43 -0500

    Crud

--- Logica/CRUD/CRUD_HorarioSupervisor.cs
+++ Logica/CRUD/CRUD_HorarioSupervisor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entidades;
+using Logica.Operaciones.AccesoPublico;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace Logica.CRUD
 {
-    internal class CRUD_HorarioSupervisor
+    public class CRUD_HorarioSupervisor : Public_Supervisores, I_CRUD<Supervisor>
     {
+        public CRUD_HorarioSupervisor()
+        {
+            
+        }
+
+        public Response<Supervisor> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Supervisor> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<Supervisor> Save(Supervisor supervisor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<Supervisor> Update(Supervisor supervisor, string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
